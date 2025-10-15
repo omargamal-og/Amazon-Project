692d8e5b976c730b5e5c641fd0bda0e8cd450b14
omarGamal <omargamalemam@gmail.com>
2025-10-16 00:12:38 +0300

    Add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,6 +1,6 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
-console.log('test suite: formatCurrency')
+console.log('test suite: formatCurrency');
 
 console.log('converts cents into dollars');
 
